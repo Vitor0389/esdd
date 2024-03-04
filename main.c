421f6798dfbe765576b4a042b3806c857115dcf8
Vitor0389 <134299283+Vitor0389@users.noreply.github.com>
2024-03-04 20:20:54 -0300

    atualizando funcoes

--- main.c
+++ main.c
@@ -1,28 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct{
+typedef struct
+{
     char magicNumber[3];
     int width, height;
     int maxGray;
-    int matrizP2[][];
+    int *matrizP2;
 } imagemP2;
 
-void lerArquivoP2(FILE *P2){
-        
-
+void alocarMatrizP2(imagemP2 *imagemPGM)
+{
+    imagemPGM->matrizP2 = calloc(sizeof(int), (imagemPGM->width * imagemPGM->height));
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
+void lerArquivoP2(FILE *P2, imagemP2 *imagemPGM)
+{
+
+    P2 = fopen("arquivoP2", 'r');
+    fscanf(P2, "%s", imagemPGM->magicNumber);
+    fscanf(P2, "%d %d", imagemPGM->width, imagemPGM->height);
+    fscanf(P2, "%d", imagemPGM->maxGray);
+
+    alocarMatrizP2(imagemPGM);
+
+    for (int i = 0; i < imagemPGM->height; i++)
+    {
+        for (int j = 0; j < imagemPGM->width; j++)
+        {
+            fscanf(P2, "%d", imagemPGM->matrizP2[i * imagemPGM->width + j]);
+        }
+    }
+}
+
+void compactar(imagemP2 *imagemPGM)
+{
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
         printf("Uso: nome_do_programa <arquivo_de_entrada> <arquivo_de_saida>\n");
         exit(1);
     }
-
+    imagemP2 *imagemPGM;
     char *entrada = argv[1];
     char *saida = argv[2];
 
-
     return 0;
 }
-
-
