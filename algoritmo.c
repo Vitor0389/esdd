b28b2e54ee537c08ee4a43f249cbd7e0ffcf5856
Vitor0389 <134299283+Vitor0389@users.noreply.github.com>
2024-03-05 17:16:46 -0300

    explicando o código

--- algoritmo.c
+++ algoritmo.c
@@ -2,12 +2,15 @@
 int main(){
     int array[12] = {1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3}; //Array de exemplo
     int *arrayCompactado;
+    int size = 0 ; //Calculando tamanho do array
     int k = 0;
+    int sizeFinal = 0;
 
-    arrayCompactado = calloc(sizeof(int), 12);
+    arrayCompactado = calloc(sizeof(int), 12); // Calloc pra nao dar lixo de memória
     int contador = 1; // contador
     for(int i = 0; i < 12; i++){
-        if(i + 1 < 12 && array[i] == array[i + 1]){
+        size ++; // Contando quantos elementos tem
+        if(i + 1 < 12 && array[i] == array[i + 1]){ // Condição de parada i + 1 < 12 pra ele nao tentar ler o que ta fora do Array.
             contador ++;
         }
         else{
@@ -18,6 +21,9 @@ int main(){
             contador = 1;
         }
     }
+    sizeFinal = size - k;
+    arrayCompactado = realloc(arrayCompactado, sizeFinal * sizeof(int));
+    
     for(int i = 0;  i < k; i++){
         printf("[%2d]", arrayCompactado[i]);
     }
